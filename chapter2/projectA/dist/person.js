d1fc4f1dcfa681a7bcfe8aaf3ddec7636f714fed
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-01 08:26:24 +0900

    learn tuples, unknown, and project references

---
+++ chapter2/projectA/dist/person.js
@@ -0,0 +1,8 @@
+import { randomString } from "../../shared/dist/utils";
+var Person = /** @class */ (function () {
+    function Person() {
+        this.id = randomString();
+    }
+    return Person;
+}());
+//# sourceMappingURL=person.js.map
