36b63831a0360831e398742c48f40fad2a221561
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-04 16:00:23 +0900

    Query parameters

--- reactshop/src/Header.tsx
+++ reactshop/src/Header.tsx
@@ -1,10 +1,36 @@
 import * as React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, RouteComponentProps, withRouter } from "react-router-dom";
+import "url-search-params-polyfill";
 import logo from "./logo.svg";
 
-const Header: React.SFC = () => {
+const Header: React.SFC<RouteComponentProps> = props => {
+  const [search, setSearch] = React.useState("");
+  React.useEffect(() => {
+    const searchParams = new URLSearchParams(props.location.search);
+    setSearch(searchParams.get("search") || "");
+  }, []);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.currentTarget.value);
+  };
+
+  const handleSearchKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      props.history.push(`/products?search=${search}`);
+    }
+  };
+
   return (
     <header className="header">
+      <div className="search-container">
+        <input
+          type="search"
+          placeholder="search"
+          value={search}
+          onChange={handleSearchChange}
+          onKeyDown={handleSearchKeydown}
+        />
+      </div>
       <img src={logo} className="header-logo" alt="logo" />
       <h1 className="header-title">React Shop</h1>
       <nav>
@@ -27,4 +53,4 @@ const Header: React.SFC = () => {
   );
 };
 
-export default Header;
+export default withRouter(Header);
--- reactshop/src/ProductsPage.tsx
+++ reactshop/src/ProductsPage.tsx
@@ -1,16 +1,32 @@
 import * as React from "react";
 import { IProduct, products } from "./ProductsData";
 import { Link } from "react-router-dom";
+import { RouteComponentProps } from "react-router-dom";
+import "url-search-params-polyfill";
 
 interface IState {
   products: IProduct[];
+  search: string;
 }
 
-class ProductsPage extends React.Component<{}, IState> {
-  public constructor(props: {}) {
+class ProductsPage extends React.Component<RouteComponentProps, IState> {
+  public static getDerivedStateFromProps(
+    props: RouteComponentProps,
+    state: IState
+  ) {
+    const searchParams = new URLSearchParams(props.location.search);
+    const search = searchParams.get("search") || "";
+    return {
+      products: state.products,
+      search
+    };
+  }
+
+  public constructor(props: RouteComponentProps) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+      search: ""
     };
   }
 
@@ -25,11 +41,23 @@ class ProductsPage extends React.Component<{}, IState> {
           Welcome to React Shop where you can get all your tools for ReactJS!
         </p>
         <ul className="product-list">
-          {this.state.products.map(product => (
-            <li key={product.id} className="product-list-item">
-              <Link to={`/products/${product.id}`}>{product.name}</Link>
-            </li>
-          ))}
+          {this.state.products.map(product => {
+            if (
+              !this.state.search ||
+              (this.state.search &&
+                product.name
+                  .toLowerCase()
+                  .indexOf(this.state.search.toLowerCase()) > -1)
+            ) {
+              return (
+                <li key={product.id} className="product-list-item">
+                  <Link to={`/products/${product.id}`}>{product.name}</Link>
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
         </ul>
       </div>
     );
