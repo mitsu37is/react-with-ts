57098f5b6bd32a4412ab6b737f22d95b43bc940f
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-04 15:20:02 +0900

    Handling not found routes

--- reactshop/src/Routes.tsx
+++ reactshop/src/Routes.tsx
@@ -1,19 +1,23 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import Header from "./Header";
 import ProductPage from "./ProductPage";
+import NotFoundPage from "./NotFoundPage";
 
 const Routes: React.SFC = () => {
   return (
     <Router>
       <div>
         <Header />
-        <Route exact={true} path="/products" component={ProductsPage} />
-        <Route path="/products/:id" component={ProductPage} />
-        <Route path="/admin" component={AdminPage} />
+        <Switch>
+          <Route exact={true} path="/products" component={ProductsPage} />
+          <Route path="/products/:id" component={ProductPage} />
+          <Route path="/admin" component={AdminPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     </Router>
   );
