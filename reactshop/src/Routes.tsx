5e2db110a80484ad3fff2124d6a9de37b278b0f8
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-04 15:32:21 +0900

    Implementing page redirects

--- reactshop/src/Routes.tsx
+++ reactshop/src/Routes.tsx
@@ -1,21 +1,32 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
 
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import Header from "./Header";
 import ProductPage from "./ProductPage";
+import LoginPage from "./LoginPage";
 import NotFoundPage from "./NotFoundPage";
 
 const Routes: React.SFC = () => {
+  const [loggedIn, setLoggedIn] = React.useState(false);
   return (
     <Router>
       <div>
         <Header />
         <Switch>
+          <Redirect exact={true} from="/" to="/products" />
           <Route exact={true} path="/products" component={ProductsPage} />
           <Route path="/products/:id" component={ProductPage} />
-          <Route path="/admin" component={AdminPage} />
+          <Route path="/admin">
+            {loggedIn ? <AdminPage /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/login" component={LoginPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
