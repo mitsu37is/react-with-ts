c6bed8378685ba9049b0020464cbd97952112cc9
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-07 15:41:52 +0900

    done chapter6

--- reactshop/src/ProductPage.tsx
+++ reactshop/src/ProductPage.tsx
@@ -1,27 +1,32 @@
 import * as React from "react";
 import { Prompt, RouteComponentProps } from "react-router-dom";
-import { IProduct, products } from "./ProductsData";
+import Product from "./Product";
+import { getProduct, IProduct } from "./ProductsData";
 
 type Props = RouteComponentProps<{ id: string }>;
 
 interface IState {
   product?: IProduct;
   added: boolean;
+  loading: boolean;
 }
 
 class ProductPage extends React.Component<Props, IState> {
   public constructor(props: Props) {
     super(props);
     this.state = {
-      added: false
+      added: false,
+      loading: true
     };
   }
 
-  public componentDidMount() {
+  public async componentDidMount() {
     if (this.props.match.params.id) {
       const id: number = parseInt(this.props.match.params.id, 10);
-      const product = products.filter(p => p.id === id)[0];
-      this.setState({ product });
+      const product = await getProduct(id);
+      if (product !== null) {
+        this.setState({ product, loading: false });
+      }
     }
   }
 
@@ -30,20 +35,13 @@ class ProductPage extends React.Component<Props, IState> {
     return (
       <div className="page-container">
         <Prompt when={!this.state.added} message={this.navAwayMessage} />
-        {product ? (
-          <React.Fragment>
-            <h1>{product.name}</h1>
-            <p>{product.description}</p>
-            <p className="product-price">
-              {new Intl.NumberFormat("en-US", {
-                currency: "USD",
-                style: "currency"
-              }).format(product.price)}
-            </p>
-            {!this.state.added && (
-              <button onClick={this.handleAddClick}>Add to basket</button>
-            )}
-          </React.Fragment>
+        {product || this.state.loading ? (
+          <Product
+            loading={this.state.loading}
+            product={product}
+            inBasket={this.state.added}
+            onAddToBasket={this.handleAddClick}
+          />
         ) : (
           <p>Product not found!</p>
         )}
--- reactshop/src/ProductsData.ts
+++ reactshop/src/ProductsData.ts
@@ -3,7 +3,23 @@ export interface IProduct {
   name: string;
   description: string;
   price: number;
+  reviews: IReview[];
 }
+
+export interface IReview {
+  comment: string;
+  reviewer: string;
+}
+
+const wait = (ms: number): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+export const getProduct = async (id: number): Promise<IProduct | null> => {
+  await wait(1000);
+  const foundProducts = products.filter(customer => customer.id === id);
+  return foundProducts.length === 0 ? null : foundProducts[0];
+};
 
 export const products: IProduct[] = [
   {
@@ -11,18 +27,48 @@ export const products: IProduct[] = [
       "A collection of navigational components that compose declaratively with your app",
     id: 1,
     name: "React Router",
-    price: 8
+    price: 8,
+    reviews: [
+      {
+        comment: "Excellent! This does everything I want",
+        reviewer: "Billy"
+      },
+      {
+        comment: "The best router I've ever worked with",
+        reviewer: "Sally"
+      }
+    ]
   },
   {
     description: "A library that helps manage state across yourapp",
     id: 2,
     name: "React Redux",
-    price: 12
+    price: 12,
+    reviews: [
+      {
+        comment: "I've found this really useful in a large app I'mworking on",
+        reviewer: "Billy"
+      },
+      {
+        comment: "A bit confusing at first but simple when you get used to it",
+        reviewer: "Sally"
+      }
+    ]
   },
   {
     description: "A library that helps you interact with a GraphQL backend",
     id: 3,
     name: "React Apollo",
-    price: 12
+    price: 12,
+    reviews: [
+      {
+        comment: "I'll never work with a REST API again!",
+        reviewer: "Billy"
+      },
+      {
+        comment: "It makes working with GraphQL backends a breeze",
+        reviewer: "Sally"
+      }
+    ]
   }
 ];
