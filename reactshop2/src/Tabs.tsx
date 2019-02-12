de14f08b168bcbc4a8f543e1071c133f0dde1cf0
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-13 07:36:03 +0900

    render props pattern

--- reactshop2/src/Tabs.tsx
+++ reactshop2/src/Tabs.tsx
@@ -3,16 +3,18 @@ import ProductsPage from "./ProductsPage";
 
 interface IState {
   activeName: string;
+  activeContent: React.ReactNode;
 }
 
 interface ITabProps {
   name: string;
   initialActive?: boolean;
+  heading: () => string | JSX.Element;
 }
 
 interface ITabsContext {
   activeName?: string;
-  handleTabClick?: (name: string) => void;
+  handleTabClick?: (name: string, content: React.ReactNode) => void;
 }
 
 const TabsContext = React.createContext<ITabsContext>({});
@@ -21,6 +23,12 @@ class Tabs extends React.Component<{}, IState> {
   public static Tab: React.SFC<ITabProps> = props => (
     <TabsContext.Consumer>
       {(context: ITabsContext) => {
+        if (!context.activeName && props.initialActive) {
+          if (context.handleTabClick) {
+            context.handleTabClick(props.name, props.children);
+            return null;
+          }
+        }
         const activeName = context.activeName
           ? context.activeName
           : props.initialActive
@@ -28,7 +36,7 @@ class Tabs extends React.Component<{}, IState> {
           : "";
         const handleTabClick = (e: React.MouseEvent<HTMLLIElement>) => {
           if (context.handleTabClick) {
-            context.handleTabClick(props.name);
+            context.handleTabClick(props.name, props.children);
           }
         };
         return (
@@ -36,7 +44,7 @@ class Tabs extends React.Component<{}, IState> {
             onClick={handleTabClick}
             className={props.name === activeName ? "active" : ""}
           >
-            {props.children}
+            {props.heading()}
           </li>
         );
       }}
@@ -52,12 +60,13 @@ class Tabs extends React.Component<{}, IState> {
         }}
       >
         <ul className="tabs">{this.props.children}</ul>
+        <div>{this.state && this.state.activeContent}</div>
       </TabsContext.Provider>
     );
   }
 
-  private handleTabClick = (name: string) => {
-    this.setState({ activeName: name });
+  private handleTabClick = (name: string, content: React.ReactNode) => {
+    this.setState({ activeName: name, activeContent: content });
   };
 }
 
