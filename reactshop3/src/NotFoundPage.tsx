af2fef07e3e1a58a2ad6996aa127dbde32a4fe6b
Hm | edihasam <masa.ishi.right@gmail.com>
2019-02-27 08:20:18 +0900

    prepare chapter6

---
+++ reactshop3/src/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="page-container">
+      <h1>Sorry, this page cannot be found</h1>
+    </div>
+  );
+};
+export default NotFoundPage;
