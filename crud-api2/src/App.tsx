523e16d42190fed58010939ad1ced269c516b567
Hm | edihasam <masa.ishi.right@gmail.com>
2019-03-06 09:38:59 +0900

    [create] crud-api2

---
+++ crud-api2/src/App.tsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+interface IPost {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+interface IState {
+  posts: IPost[];
+}
+
+class App extends Component<{}, IState> {
+  public constructor(props: {}) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  public componentDidMount() {
+    axios
+      .get<IPost[]>("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        this.setState({ posts: response.data });
+      });
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <ul className="posts">
+          {this.state.posts.map(post => (
+            <li key={post.id}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
