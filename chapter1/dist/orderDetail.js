c9a522ab120f8b1bfc66890675635934e304be30
Hm | edihasam <masa.ishi.right@gmail.com>
2019-03-08 10:49:01 +0900

    prepare chapter4 and chapter 9

--- chapter1/dist/orderDetail.js
+++
@@ -1,8 +0,0 @@
-class orderDetail {
-    getTotal(discount) {
-        const priceWithoutDiscount = this.product.unitPrice * this.quantity;
-        const discountAmount = priceWithoutDiscount * discount;
-        return priceWithoutDiscount - discountAmount;
-    }
-}
-//# sourceMappingURL=orderDetail.js.map
--- chapter2/ProjectA/src/person.ts
+++
@@ -1,8 +0,0 @@
-import { randomString } from "../../Shared/dist/utils";
-class Person {
-  id: string;
-  name: string;
-  constructor() {
-    this.id = randomString();
-  }
-}
